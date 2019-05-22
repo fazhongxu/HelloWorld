ed2b02da03ddc176c07b7fa9b0e4b9a48b4ac4ae
fazhongxu <Julian@JuliandeMacBook-Pro.local>
2019-05-22 16:14:24 +0800

    ARouter 的 @Autowired 使用

--- modules/module_web/src/main/java/com/example/xxl/module_web/WebViewActivity.java
+++ modules/module_web/src/main/java/com/example/xxl/module_web/WebViewActivity.java
@@ -4,18 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.xxl.mediator_web.IConstantWeb;
 
 @Route(path = IConstantWeb.WEB_WEBVIEW)
 public class WebViewActivity extends AppCompatActivity {
+    @Autowired(name = IConstantWeb.KEY_URL)
+    String mUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-        String url = getIntent().getStringExtra(IConstantWeb.KEY_URL);
+
+        // inject 之后 通过  @Autowired 注解获取值 代替 getIntent 方式获取的值
+        ARouter.getInstance().inject(this);
+        //String url = getIntent().getStringExtra(IConstantWeb.KEY_URL);
         TextView webUrlTv = findViewById(R.id.web_url_tv);
-        webUrlTv.setText(url);
+        webUrlTv.setText(mUrl);
     }
 }
